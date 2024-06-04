57ba9302fc2117586b5db20b07cab69de77939d9
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 16:40:16 +0200

    done

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 function convertToMinutes() {
     const minutes = document.getElementById('Minutes').value;
-    const seconds = (minutes * 60);
+    const seconds = (minutes * 60); //mins to secs formula
     document.getElementById('result').innerHTML = minutes + "min is equal to    " + seconds.toFixed(2) + "s";
 }
